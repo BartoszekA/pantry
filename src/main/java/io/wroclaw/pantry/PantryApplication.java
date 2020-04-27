d93edd61c60fe70b127d2916882f3ef597b9e74c
BartoszekA <agnieszka.bartoszek@gmail.com>
2020-04-27 23:19:05 +0200

    first commit

---
+++ src/main/java/io/wroclaw/pantry/PantryApplication.java
@@ -0,0 +1,13 @@
+package io.wroclaw.pantry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PantryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PantryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/wroclaw/pantry/PantryApplicationTests.java
@@ -0,0 +1,13 @@
+package io.wroclaw.pantry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PantryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
