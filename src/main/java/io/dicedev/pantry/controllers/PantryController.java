0d0bd75780fa6f6a25798a6412858fa393b8d442
BartoszekA <agnieszka.bartoszek@gmail.com>
2020-05-03 13:29:39 +0200

    pantry controller get & post updated

--- src/main/java/io/dicedev/pantry/controllers/PantryController.java
+++ src/main/java/io/dicedev/pantry/controllers/PantryController.java
@@ -1,23 +1,28 @@
 package io.dicedev.pantry.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class PantryController {
 
-    private String product;
+    private List<String> products = new ArrayList<>();
 
-    @GetMapping("/product")
-    public String getProduct() {
-        return product;
+    @GetMapping("/product/{id}")
+    public String getProduct(@PathVariable int id) {
+        return products.get(id+1);
+    }
+
+    @GetMapping("/products")
+    public String getProducts() {
+        return products.toString();
     }
 
     @PostMapping("/product")
     public void addProduct(@RequestBody String product) {
-        this.product = product;
+        products.add(product);
         System.out.println(product);
     }
 }
