0f81316a9177c46e74ce5ca4c2f9a417fe3a51b7
BartoszekA <agnieszka.bartoszek@gmail.com>
2020-06-18 08:18:05 +0200

    adding new product categories

--- src/main/java/io/dicedev/pantry/domain/enums/ProductCategoryEnum.java
+++ src/main/java/io/dicedev/pantry/domain/enums/ProductCategoryEnum.java
@@ -8,7 +8,11 @@ import lombok.Getter;
 public enum ProductCategoryEnum {
     DAIRY("Dairy", 1),
     FRUITS_AND_VEGETABLES("Fruits and vegetables", 2),
-    LOOSE_PRODUCTS("Loose products", 3);
+    LOOSE_PRODUCTS("Loose products", 3),
+    BREAD("Bread", 4),
+    SWEETS("Sweets", 5),
+    COFFEE_AND_TEA("Coffee and tea", 6),
+    CANNED_FOOD("Canned food", 7);
 
     final private String name;
     final private Integer id;
